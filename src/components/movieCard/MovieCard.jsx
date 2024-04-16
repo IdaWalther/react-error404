825245a872978839467d29c67309ee6ab750f3ce
Magnus Nilsson <regnhundar@gmail.com>
2024-04-16 14:03:47 +0200

    lade till css för moviecard

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -1,12 +1,12 @@
+import "./movieCard.css"
 
-
-function MovieCard({poster, title, key}) {
-  return (
-    <figure className="movie-card" key={key}>
-        <img src={poster} alt={`Poster of the movie ${title}`} className="movie-card__poster" />
-        <figcaption className="movie-card__title">{title}</figcaption>
-    </figure>
-  )
+function MovieCard({ poster, title }) {
+    return (
+        <figure className="movie-card">
+            <figcaption className="movie-card__title">{title}</figcaption>
+            <img src={poster} alt={`Poster of the movie ${title}`} className="movie-card__poster" />
+        </figure>
+    )
 }
 
-export default MovieCard
+export default MovieCard;
